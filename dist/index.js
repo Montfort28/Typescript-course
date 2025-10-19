c16dfb62be100312179761841361c052a62bf2d4
Montfort <mugishamontfort28@gmail.com>
2025-10-19 23:52:53 +0200

    adding a tsconfig file for easier compiling

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let age = 30;
+let FirstName = 'Montfort';
+let isFictional = true;
+console.log(age, FirstName, isFictional);
+//# sourceMappingURL=index.js.map
--- index.js
+++
@@ -1,4 +0,0 @@
-var age = 30;
-var FirstName = 'Montfort';
-var isFictional = true;
-console.log(age, FirstName, isFictional);
