90945d884c38d75c004beac2f6545c1da3c6ef15
Montfort <mugishamontfort28@gmail.com>
2025-10-19 23:56:36 +0200

    null and undefined types

--- dist/index.js
+++ dist/index.js
@@ -3,5 +3,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 let age = 30;
 let FirstName = 'Montfort';
 let isFictional = true;
+let number;
+let ageing;
+number = null;
+ageing = undefined;
 console.log(age, FirstName, isFictional);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
 let age: number = 30
 let FirstName: string = 'Montfort'
 let isFictional: boolean = true
+let number: null
+let ageing: undefined
+
+number = null
+ageing = undefined
 
 console.log(age, FirstName, isFictional)
