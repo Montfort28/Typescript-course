67c64884a0cc397f5d1d1ce3febebc114feb0b56
Montfort <mugishamontfort28@gmail.com>
2025-10-21 23:37:13 +0200

    arrays

--- dist/index.js
+++ dist/index.js
@@ -3,5 +3,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 let age = 30;
 let FirstName = 'Montfort';
 let isFictional = true;
+let names;
 console.log(age, FirstName);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 let age: number = 30
 let FirstName: string = 'Montfort'
 let isFictional: boolean = true
+let names: [string]
 
 console.log(age, FirstName)
