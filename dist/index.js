3a2fe04847d55350f01f2fabd8ebd22c67e43fbe
Montfort <mugishamontfort28@gmail.com>
2025-10-22 00:22:47 +0200

    different example on normal functions

--- dist/index.js
+++ dist/index.js
@@ -4,8 +4,14 @@ Object.defineProperty(exports, "__esModule", { value: true });
 function addTwoNumbers(a, b) {
     return a + b;
 }
+addTwoNumbers(3, 9);
 //arrow functions
 const subtractTwoNumbers = (a, b) => {
     return a - b;
 };
+function addAllNumbers(items) {
+    const total = items.reduce((a, c) => a + c, 0);
+    console.log(total);
+}
+addAllNumbers([5, 7, 9, 2, 0, 67]);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,17 @@ function addTwoNumbers (a: number, b: number): number{
     return a+b
 }
 
+addTwoNumbers(3, 9)
+
 //arrow functions
 
 const subtractTwoNumbers = (a: number, b: number): number =>{
     return a-b
 }
+
+function addAllNumbers( items: number[]){
+    const total = items.reduce((a, c) => a + c, 0)
+    console.log(total)
+}
+
+addAllNumbers([5,7,9,2,0,67])
