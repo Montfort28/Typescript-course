dbfda577e8ab22560f24e8c91b038d3422fbecc6
Montfort <mugishamontfort28@gmail.com>
2025-10-22 00:10:04 +0200

    normal functions

--- dist/index.js
+++ dist/index.js
@@ -1,31 +1,8 @@
 "use strict";
-// let age: number = 30
-// let FirstName: string = 'Montfort'
-// let isFictional: boolean = true
+//normal functions
 Object.defineProperty(exports, "__esModule", { value: true });
-// console.log(age, FirstName)
-//arrays
-let names;
-//type infering
-let ages = [12, 15, 18];
-const age = ages[2];
-console.log(age);
-let types = [35, true, 'hello'];
-const T = types[1];
-console.log(T);
-//object literals
-let user = {
-    firstName: 'luigi',
-    age: 23,
-    id: 1
-};
-user.firstName = 'mont';
-user.age = 27;
-user.id = 2;
-//type inference with object literals
-let person = {
-    name: 'mugisha',
-    score: 35
-};
-const P = person.name;
+function addTwoNumbers(a, b) {
+    return a + b;
+}
+//arrow functions
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,8 @@
-// let age: number = 30
-// let FirstName: string = 'Montfort'
-// let isFictional: boolean = true
+//normal functions
 
-// console.log(age, FirstName)
-
-//arrays
-
-let names: [string]
-
-//type infering
-
-let ages =[12, 15, 18]
-const age = ages[2]
-console.log(age)
-
-let types = [35, true, 'hello']
-const T = types[1]
-console.log(T)
-
-
-//object literals
-
-let user: {firstName: string, age: number, id: number} = {
-    firstName: 'luigi',
-    age: 23,
-    id: 1
+function addTwoNumbers (a: number, b: number): number{
+    return a+b
 }
 
-user.firstName = 'mont'
-user.age = 27
-user.id = 2
+//arrow functions
 
-//type inference with object literals
-
-let person = {
-    name: 'mugisha',
-    score: 35
-}
-
-const P = person.name
