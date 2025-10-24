1317ba74cef3729a18bbf0d69592019555dfa526
Montfort <mugishamontfort28@gmail.com>
2025-10-24 23:49:42 +0200

    type eliases example

--- dist/index.js
+++ dist/index.js
@@ -59,5 +59,13 @@ createPost(newPost);
 //arrays
 let posts = [];
 posts.push(newPost);
-//type eliases
+function getRandomColor() {
+    const r = Math.floor(Math.random() * 255);
+    const g = Math.floor(Math.random() * 255);
+    const b = Math.floor(Math.random() * 255);
+    return [r, g, b];
+}
+const colorOne = getRandomColor();
+const colorTwo = getRandomColor();
+console.log(colorOne, colorTwo);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -109,3 +109,17 @@ posts.push(newPost)
 
 //type eliases
 
+type Rgb = [number, number, number]
+
+function getRandomColor(): Rgb{
+
+    const r = Math.floor(Math.random() *255)
+    const g = Math.floor(Math.random() *255)
+    const b = Math.floor(Math.random() *255)
+
+    return [r, g, b]
+}
+
+const colorOne = getRandomColor()
+const colorTwo = getRandomColor()
+console.log(colorOne, colorTwo)
